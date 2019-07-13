79daeddcab5f2939047de3ea5c190559410d6383
Nyota254 <knyota66@gmail.com>
2019-07-13 14:47:50 +0300

    add changes to html file from checkbox to radio button

--- js/script.js
+++ js/script.js
@@ -5,35 +5,35 @@ return (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
 
 function dayandAkanname(gender, dayweekofBirth){
 if (gender === "male" && dayweekofBirth === 2 ){
-	alert("You were born on SUNDAY and your AKAN name is KWASI.")
+	var name = "You were born on SUNDAY and your AKAN name is KWASI."
 } else if(gender === "male" && dayweekofBirth === 3 ) {
-	alert("you were born on MONDAY and your AKAN name is KWADWO.")
+	var name = "you were born on MONDAY and your AKAN name is KWADWO."
 } else if(gender === "male" && dayweekofBirth === 4 ) {
-	alert("you were born on TUESDAY and your AKAN name is KWABENA.")
+	var name = "you were born on TUESDAY and your AKAN name is KWABENA."
 } else if(gender === "male" && dayweekofBirth === 5 ) {
-	alert("you were born on WEDNESDAY and your AKAN name is KWAKU.")
+	var name = "you were born on WEDNESDAY and your AKAN name is KWAKU."
 } else if(gender === "male" && dayweekofBirth === 6) {
-	alert("you were born on THURSDAY and your AKAN name is YAW.")
+	var name = "you were born on THURSDAY and your AKAN name is YAW."
 } else if(gender === "male" && dayweekofBirth === 7) {
-	alert("you were born on FRIDAY and your AKAN name is KOFI.")
+	var name = "you were born on FRIDAY and your AKAN name is KOFI."
 } else if(gender === "male" && dayweekofBirth === 1) {
-	alert("you were born on SATURDAY and your AKAN name is KWAME.")
+	var name = "you were born on SATURDAY and your AKAN name is KWAME."
 } else if(gender === "female" && dayweekofBirth === 2) {
-	alert("you were born on SUNDAY and your AKAN name is AKOSUA.")
+	var name = "you were born on SUNDAY and your AKAN name is AKOSUA."
 } else if(gender === "female" && dayweekofBirth === 3) {
-	alert("you were born on MONDAY and your AKAN name is ADWOA.")
+	var name = "you were born on MONDAY and your AKAN name is ADWOA."
 } else if(gender === "female" && dayweekofBirth === 4) {
-	alert("you were born on TUESDAY and your AKAN name is ABENAA.")
+	var name = "you were born on TUESDAY and your AKAN name is ABENAA."
 } else if(gender === "female" && dayweekofBirth === 5) {
-	alert("you were born on WEDNESDAY and your AKAN name is AKUA.")
+	var name = "you were born on WEDNESDAY and your AKAN name is AKUA."
 } else if(gender === "female" && dayweekofBirth === 6) {
-	alert("you were born on THURSDAY and your AKAN name is YAA.")
+	var name = "you were born on THURSDAY and your AKAN name is YAA."
 } else if(gender === "female" && dayweekofBirth === 7) {
-	alert("you were born on FRIDAY and your AKAN name is AFUA.")
+	var name = "you were born on FRIDAY and your AKAN name is AFUA."
 } else if(gender === "female" && dayweekofBirth === 1) {
-	alert("you were born on SATURDAY and your AKAN name is AMA.")
+	var name = "you were born on SATURDAY and your AKAN name is AMA."
 } else{
-  alert("INVALID ENTRY")
+  var name = "INVALID ENTRY"
 }}
 
 
@@ -48,6 +48,9 @@ var yearofBirth = parseInt($("#yearofBirth").val());
 var gender = $("#gender").val();
 var resultOne = weekDayCalculator(centuryofBirth, yearofBirth, monthofBirth, dayofBirth);
 var result = resultOne.toFixed();
+var resultsTwo = dayandAkanname(gender, result);
 $("#output").text(result);
+$("#output").text(name);
+
 });
 });
