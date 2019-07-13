a309a0e85ed03f2283aea17a44a6cdf258956e1c
Nyota254 <knyota66@gmail.com>
2019-07-13 20:25:18 +0300

    add more javascript functionality for displaying akan name

--- js/script.js
+++ js/script.js
@@ -3,38 +3,46 @@ return (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
 };
 
 
-function dayandAkanname(gender, dayweekofBirth){
-if (gender === "male" && dayweekofBirth === 2 ){
-	var name = "You were born on SUNDAY and your AKAN name is KWASI."
-} else if(gender === "male" && dayweekofBirth === 3 ) {
-	var name = "you were born on MONDAY and your AKAN name is KWADWO."
-} else if(gender === "male" && dayweekofBirth === 4 ) {
-	var name = "you were born on TUESDAY and your AKAN name is KWABENA."
-} else if(gender === "male" && dayweekofBirth === 5 ) {
-	var name = "you were born on WEDNESDAY and your AKAN name is KWAKU."
-} else if(gender === "male" && dayweekofBirth === 6) {
-	var name = "you were born on THURSDAY and your AKAN name is YAW."
-} else if(gender === "male" && dayweekofBirth === 7) {
-	var name = "you were born on FRIDAY and your AKAN name is KOFI."
-} else if(gender === "male" && dayweekofBirth === 1) {
-	var name = "you were born on SATURDAY and your AKAN name is KWAME."
-} else if(gender === "female" && dayweekofBirth === 2) {
-	var name = "you were born on SUNDAY and your AKAN name is AKOSUA."
-} else if(gender === "female" && dayweekofBirth === 3) {
-	var name = "you were born on MONDAY and your AKAN name is ADWOA."
-} else if(gender === "female" && dayweekofBirth === 4) {
-	var name = "you were born on TUESDAY and your AKAN name is ABENAA."
-} else if(gender === "female" && dayweekofBirth === 5) {
-	var name = "you were born on WEDNESDAY and your AKAN name is AKUA."
-} else if(gender === "female" && dayweekofBirth === 6) {
-	var name = "you were born on THURSDAY and your AKAN name is YAA."
-} else if(gender === "female" && dayweekofBirth === 7) {
-	var name = "you were born on FRIDAY and your AKAN name is AFUA."
-} else if(gender === "female" && dayweekofBirth === 1) {
-	var name = "you were born on SATURDAY and your AKAN name is AMA."
-} else{
-  var name = "INVALID ENTRY"
-}}
+var dayandAkanname = function(genderOne, dayweekofBirth){
+if (genderOne == "male" && dayweekofBirth == 2 ){
+	return $("#output2").text("You were born on SUNDAY and your AKAN name is KWASI.");
+}  if(genderOne == "male" && dayweekofBirth == 3 ) {
+	return $("#output2").text("you were born on MONDAY and your AKAN name is KWADWO.");
+}  if(genderOne == "male" && dayweekofBirth == 4 ) {
+	return $("#output2").text("you were born on TUESDAY and your AKAN name is KWABENA.");
+}  if(genderOne == "male" && dayweekofBirth == 5 ) {
+	return $("#output2").text("you were born on WEDNESDAY and your AKAN name is KWAKU.");
+}  if(genderOne == "male" && dayweekofBirth == 6) {
+	return $("#output2").text("you were born on THURSDAY and your AKAN name is YAW.");
+}  if(genderOne == "male" && dayweekofBirth == 7) {
+	return $("#output2").text("you were born on FRIDAY and your AKAN name is KOFI.");
+}  if(genderOne == "male" && dayweekofBirth == 1) {
+	return $("#output2").text("you were born on SATURDAY and your AKAN name is KWAME.");
+}  if(genderOne == "female" && dayweekofBirth == 2) {
+	return $("#output2").text("you were born on SUNDAY and your AKAN name is AKOSUA.");
+}  if(genderOne == "female" && dayweekofBirth == 3) {
+	return $("#output2").text("you were born on MONDAY and your AKAN name is ADWOA.");
+}  if(genderOne == "female" && dayweekofBirth == 4) {
+	return $("#output2").text("you were born on TUESDAY and your AKAN name is ABENAA.");
+}  if(genderOne == "female" && dayweekofBirth == 5) {
+	return $("#output2").text("you were born on WEDNESDAY and your AKAN name is AKUA.");
+}  if(genderOne == "female" && dayweekofBirth == 6) {
+	return $("#output2").text("you were born on THURSDAY and your AKAN name is YAA.");
+}  if(genderOne == "female" && dayweekofBirth == 7) {
+	return $("#output2").text("you were born on FRIDAY and your AKAN name is AFUA.");
+}  if(genderOne == "female" && dayweekofBirth == 1) {
+	return $("#output2").text("you were born on SATURDAY and your AKAN name is AMA.");
+} };
+
+function validateData(dayofBirth, monthofBirth){
+ if(dayofBirth <= 0 || dayofBirth > 31){
+ alert ("INVALID DAY OF BIRTH PLEASE ENTER A NUMBER BETWEEN 1 AND 31");
+ return;
+ }
+ if(monthofBirth <= 0 || monthofBirth > 12) {
+ alert ("INVALID MONTH PLEASE ENTER A NUMBER BETWEEN 1 AND 12")
+ return;
+}};
 
 
 $(document).ready(function() {
@@ -45,13 +53,11 @@ var monthofBirth = parseInt($("#monthofBirth").val());
 var fullyearofBirth = parseInt($("#fullyearofBirth").val());
 var centuryofBirth = parseInt($("#centuryofBirth").val());
 var yearofBirth = parseInt($("#yearofBirth").val());
-var gender = $("#male").val();
-var gender = $("#female").val();
+var gender = $("input:radio[name=gender]:checked").val();
+validateData(dayofBirth, monthofBirth);
 var resultOne = weekDayCalculator(centuryofBirth, yearofBirth, monthofBirth, dayofBirth);
 var result = resultOne.toFixed();
-var resultsTwo = dayandAkanname(gender, result);
-$("#output").text(result);
-$("#output").text(name);
-
+$("#output").text(gender);
+dayandAkanname(gender, result);
 });
 });
